5e09c62a98ca6aa3185201c765aa8130bb84ab50
Souheil CHELFOUH <trollfot@gmail.com>
2011-04-29 20:06:44 +0200

    Added specific tests for security.

--- src/dolmen/view/tests_nosecurity/view/missingcontext.py
+++ src/dolmen/view/tests_nosecurity/view/missingcontext.py
@@ -5,7 +5,7 @@ Views without a context cannot be grokked:
   Traceback (most recent call last):
     ...
   GrokError: No module-level context for
-  <class 'dolmen.view.tests.view.missingcontext.Club'>, please use the
+  <class 'dolmen.view.tests_nosecurity.view.missingcontext.Club'>, please use the
   'context' directive.
 
 """
