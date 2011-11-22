6b1f6403b617a082b5e10310c404875c8281d33e
Souheil CHELFOUH <trollfot@gmail.com>
2011-11-22 18:11:17 +0100

    Moved i18n mechanisms in cromlech.browser

--- src/dolmen/view/components.py
+++ src/dolmen/view/components.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from cromlech.browser.interfaces import IView
+from cromlech.browser import IView, negotiate
 from cromlech.io import IRequest
 from grokcore.component import baseclass, implements
 from zope.component import getMultiAdapter
@@ -32,6 +32,10 @@ class View(Location):
     def update(self, *args, **kwargs):
         self.response = self.responseFactory()
 
+    @property
+    def target_language(self):
+        return negotiate(self.request)
+
     def render(self, *args, **kwargs):
         """This is the default render method.
         Not providing a template will make it fails.
@@ -39,7 +43,7 @@ class View(Location):
         """
         if self.template is None:
             raise NotImplementedError("Template is not defined.")
-        return self.template.render(self)
+        return self.template.render(self, target_language=self.target_language)
 
     def __call__(self, *args, **kwargs):
         self.update(*args, **kwargs)
